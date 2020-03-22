bb7a6ac5b29c48774414a946b0b92472b59e7868
Arad a <arad01245@gmail.com>
2020-03-22 23:49:01 +0200

    XSS vulnerability added via the contact us function

--- Server/static/js/contact.js
+++ Server/static/js/contact.js
@@ -1,85 +1,59 @@
-$(document).ready(function(){
-    
-    (function($) {
-        "use strict";
+$(document).ready(function () {
 
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+    // var subject = window.atob(getUrlVars()["subject"]);
+
+    // if ((subject != null) && (subject != "")) {
+    //     alert("hello!");
+    //     $("#messageSent").html("The message about " + subject + " was successfully transmitted");
+    // }
+
+
+    // HTML5 automatically adds the attribute 'novalidate=novalidate'     <- Maybe this is a hint?
+    $('#contactForm').removeAttr('novalidate');
 
     // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
+    $('#contactForm').validate({
+        rules: {
+            subject: {
+                required: true,
+                minlength: 4
             },
-            messages: {
-                name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
-                },
-                number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
+            message: {
+                required: true,
+                minlength: 20
             }
-        })
+        },
+        messages: {
+            subject: {
+                required: "come on, you have a subject, don't you?",
+                minlength: "your subject must consist of at least 4 characters"
+            },
+            message: {
+                required: "um...yea, you have to write something to send this form.",
+                minlength: "thats all? really?"
+            }
+        },
+        submitHandler: function (form) {
+            sendMessage($('#subject').text());
+        }
     })
-        
- })(jQuery)
+
+    $('#submit').click(function () {
+        $("#contactForm").valid();
+    });
 })
+
+var getUrlVars = function () {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+var sendMessage = function (subject) {
+    if ((subject != null) && (subject != "")) {
+        alert("hello!");
+        $("#messageSent").html("The message about " + subject + " was successfully transmitted");
+    }
+}
