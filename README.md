f0f6be391eee6e2762e55919df5a2fbad02eed3d
ori rinat <oririnat333@gmail.com>
2020-03-24 13:47:31 +0200

    improv backend change the authentication method to token base autentication

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # sample-web-CTF
 public sample web CTF, in this CTF you will face with web vulnerabilities from the concepts of : authentication, access control, session management, input handling - XSS &amp; SQL injection and certificates
 
-authentication
-hash to passwords
-obfuscation to client code
-cross site forgery attack
+- Authentication
+	- Hash to passwords
+	- Add token base authentication
+	- Add JWT
+- Obfuscation to client code
+- Cross site forgery attack
+- Prevent Brute-Force Attacks
+- XSS
+- Access control
+- Session management
+- add ssl to site
+
+Session IDs exposed on URL can lead to session fixation attack.
+Session IDs same before and after logout and login.
+Session Timeouts are not implemented correctly.
+Application is assigning same session ID for each new session.
+Authenticated parts of the application are protected using SSL and passwords are stored in hashed or encrypted format.
+The session can be reused by a low privileged user.
--- Server/web-CTF-server.py
+++ Server/web-CTF-server.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for,make_response
 import csv
-from datetime import timedelta
+from datetime import timedelta, date
 
 app = Flask(__name__)
 
@@ -10,82 +10,104 @@ def register_to_system():
 	form_password = request.form["pass"]
 	if(not isUserNameExist(form_username)):
 		users_file = open('data/users.csv', 'a')
-		users_file.write(form_username + "," + form_password + ", 0\n")
+		users_file.write(form_username + "," + form_password + "," + str(nextUserId()) + "\n")
 		users_file.close()
+		accounts_file = open('data/accounts.csv', 'a')
+		accounts_file.write(str(nextUserId() - 1) + ",0," + str(date.today()) + "," + form_username +"\n")
+		accounts_file.close()
 		return redirect(url_for('login'))
 	return render_template("register.html", money=getUserUsernameAndMoney(), invalid='Invalid user username')
 
 @app.route("/try_login_to_page", methods=["post"])
 def try_login_to_page():
-	session['username'] = request.form["username"]
-	session['pass'] = request.form["pass"]
-	if(isUserLogin()):
-		return redirect(url_for('index'))
-	else:
-		return redirect(url_for('login'))
-
-def isUserLogin():
-	if 'username' not in session or 'pass' not in session:
-		return False
 	with open('data/users.csv', 'r') as file:
 		reader = csv.reader(file)
 		for row in reader:
-			if(row[0] == session['username'] and row[1] == session['pass']):
-				return True;
+			if(row[0] == request.form["username"] and row[1] == request.form["pass"]):
+				response = make_response(redirect('/'))
+				response.set_cookie('token', value=row[2], max_age=None)
+				return response
+	return render_template("register.html", money=getUserUsernameAndMoney(), invalid='Error')
+
+def isUserLogin():
+	if not request.cookies.get('token'):
+		return False
+	else:
+		with open('data/accounts.csv', 'r') as accounts:
+			accountsReader = csv.reader(accounts)
+			for account in accountsReader:
+				if(account[0] == request.cookies.get('token')):
+					return True;
 	return False
+
+def nextUserId():
+	lastRow = None
+	with open('data/users.csv', 'r') as file:
+		reader = csv.reader(file)
+		for row in reader:
+			lastRow = row
+	if(lastRow != None):
+		return int(lastRow[2]) + 1
+	return 0
 
 def isUserNameExist(username):
 	with open('data/users.csv', 'r') as file:
 		reader = csv.reader(file)
 		for row in reader:
-			if(row[0] == username):
+			if((row is not None) and (row[0] == username)):
 				return True;
 	return False
 
-def addMoney(username, amoutToAdd):
-	usersFile = csv.reader(open('data/users.csv'))
-	usersLines = list(usersFile)
-	for i in range(len(usersLines)):
-		if(usersLines[i][0] == username):
-			if ((int(usersLines[i][2]) + int(amoutToAdd)) < 0):
+def addMoney(tokenId, amoutToAdd):
+	accountsLines = read_file('data/accounts.csv')
+	for j in range(len(accountsLines)):
+		if(accountsLines[j][0] == tokenId):
+			if ((int(accountsLines[j][1]) + int(amoutToAdd)) < 0):
 				return False
-			usersLines[i][2] = int(usersLines[i][2]) + int(amoutToAdd)
-	writer = csv.writer(open('data/users.csv', 'w'))
-	writer.writerows(usersLines)
+			accountsLines[j][1] = int(accountsLines[j][1]) + int(amoutToAdd)
+	writer = csv.writer(open('data/accounts.csv', 'w'))
+	writer.writerows(accountsLines)
 	return True
 
+def read_file(file):
+    with open(file, 'r') as f:
+        data = [row for row in csv.reader(f.read().splitlines())]
+    return data
+
 def getUserUsernameAndMoney():
-	if 'username' not in session:
+	if not request.cookies.get('token'):
+		return False
+	else:
+		accountsLines = read_file('data/accounts.csv')
+		for i in range(len(accountsLines)):
+			if(accountsLines[i][0] == request.cookies.get('token')):
+				return accountsLines[i][3] + " has " + accountsLines[i][1]
 		return 0
+
+def getIdFromUsername(username):
 	with open('data/users.csv', 'r') as file:
 		reader = csv.reader(file)
 		for row in reader:
-			if(row[0] == session['username']):
-				return row[0] + " has " + row[2]
-
-	usersFile = csv.reader(open('data/users.csv'))
-	usersLines = list(usersFile)
-	for i in range(len(usersLines)):
-		return usersLines[i][2]
+			if(row[0] == username):
+				return row[2]
 
 @app.route("/transfer-money", methods=["post"])
 def transfer_money():
-	if 'username' not in session:
-		return render_template("login.html")
-
-	form_sendTo = request.form["sendTo"]
-	form_amount = request.form["amount"]
-
-	if(not isUserNameExist(form_sendTo)):
-		return render_template("index.html", money=getUserUsernameAndMoney(), transferMoneyMessage="There is no such user name")
-
-	if(addMoney(session['username'], -1 * int(form_amount))):
-		addMoney(form_sendTo, form_amount)
-		return render_template("index.html", money=getUserUsernameAndMoney(), transferMoneyMessage="Money transfered successfully")
-
+	if not request.cookies.get('token'):
+			return render_template("login.html")
 	else:
-		return render_template("index.html", money=getUserUsernameAndMoney(), transferMoneyMessage="You dont have enough money")
+		form_sendTo = request.form["sendTo"]
+		form_amount = request.form["amount"]
 
+		if(not isUserNameExist(form_sendTo)):
+			return render_template("index.html", money=getUserUsernameAndMoney(), transferMoneyMessage="There is no such user name")
+
+		if(addMoney(request.cookies.get('token'), -1 * int(form_amount))):
+			addMoney(getIdFromUsername(form_sendTo), form_amount)
+			return render_template("index.html", money=getUserUsernameAndMoney(), transferMoneyMessage="Money transfered successfully")
+
+		else:
+			return render_template("index.html", money=getUserUsernameAndMoney(), transferMoneyMessage="You dont have enough money")
 
 @app.route("/register")
 def register():
@@ -93,18 +115,15 @@ def register():
 
 @app.route("/login", methods=["get"])
 def login():
-	if(isUserLogin()):
-		return redirect(url_for('/'))
-	else:
-		return render_template("login.html")
+	return render_template("login.html")
 
 @app.route("/logout")
 def logout():
-	if 'username' not in session or 'pass' not in session:
-		return render_template("login.html")
-	session.pop('username')
-	session.pop('pass')
-	return render_template("login.html")
+	if not request.cookies.get('token'):
+			return render_template("login.html")
+	resp = make_response(render_template("login.html"))
+	resp.set_cookie('token', expires=0)
+	return resp
 
 @app.route("/")
 def home():
@@ -117,6 +136,13 @@ def home():
 def index():
 	if(isUserLogin()):
 		return render_template("index.html", money=getUserUsernameAndMoney())
+	else:
+		return render_template("login.html")
+
+@app.route("/my_account")
+def my_account():
+	if(isUserLogin()):
+		return render_template("my_account.html", money=getUserUsernameAndMoney())
 	else:
 		return render_template("login.html")
 
@@ -169,16 +195,8 @@ def contact():
 	else:
 		return render_template("login.html")
 
-@app.before_request
-def make_session_permanent():
-    session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=2)
-
 if __name__ == "__main__":
 	app.secret_key = 'super secret key'
 	app.config['SESSION_TYPE'] = 'filesystem'
-	username = 'admin'
-	password = 'admin'
-	users = ""
 
 	app.run(debug=True)
